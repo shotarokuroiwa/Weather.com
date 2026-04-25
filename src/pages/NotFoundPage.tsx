ce7ce628eb9456084285fb45be78a8ee5aa36946
shotarokuroiwa <shotarokuroiwa@icloud.com>
2026-04-25 18:52:43 +0900

    feat: NotFound実装

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import { useNavigate } from 'react-router-dom'
+
 const NotFoundPage = () => {
-  return <div>NotFound</div>
+  const navigate = useNavigate()
+  return (
+    <div>
+      <h1>404 - ページが見つかりませんでした</h1>
+      <p>お探しのページは存在しないか、移動した可能性があります</p>
+      <button type="button" onClick={() => navigate('/')}>
+        トップページに戻る
+      </button>
+    </div>
+  )
 }
 
 export default NotFoundPage;
+
